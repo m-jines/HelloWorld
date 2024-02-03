1e4c471a3ff97f697ffb270fd91ba43e32413ce1
Mary Jines <143438544+m-jines@users.noreply.github.com>
2024-02-03 17:04:28 -0600

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -9,7 +9,7 @@ http.createServer(function (request, response) {
 
    // Send the response body as "Hello World"
    response.end('Yeehaw!\n')
-}).listen(3334)
+}).listen(3333)
 
 // Console will print the message
 console.log('Server running')
